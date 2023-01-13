ff7b770f0492c70ea463e5575fad7e16511eee9e
edi-sipka@gmail.com <edisipka@gmail.com>
2023-01-13 14:28:52 +0100

    Update routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,11 @@
 Rails.application.routes.draw do
   mount Rswag::Ui::Engine => '/api-docs'
   mount Rswag::Api::Engine => '/api-docs'
-  devise_for :users
+  devise_for :users,
+  controllers: {
+      sessions: 'users/sessions',
+      registrations: 'users/registrations'
+  }
   namespace :api do
     namespace :v1 do
       resources :cars

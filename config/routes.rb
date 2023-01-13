732a14df3bb516608f0ed1ac4034cfd6e21d8ca6
Lena <summyalena@gmail.com>
2023-01-13 10:55:41 +0100

    added routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,10 @@ Rails.application.routes.draw do
   mount Rswag::Ui::Engine => '/api-docs'
   mount Rswag::Api::Engine => '/api-docs'
   devise_for :users
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-  # root "articles#index"
+  namespace :api do
+    namespace :v1 do
+      resources :cars
+      resources :reservations
+    end
+  end
 end

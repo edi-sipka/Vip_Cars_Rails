9cc3ef6cb33860e60c3667b05e1d8c886122a99f
edi-sipka@gmail.com <edisipka@gmail.com>
2023-01-13 14:48:03 +0100

    Add 2 cars in db:seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,9 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
-#   Character.create(name: "Luke", movie: movies.first)
+cars = Car.create([
+    {
+      name: 'BMW F10',
+      image: 'https://www.pngmart.com/files/22/BMW-F10-PNG-HD-Isolated.png',
+      model: '2021',
+      price: 140,
+      description: 'The F10 is the model code BMW uses internally for the sixth-generation 5 Series sedan, produced between 2010 and 2016.'
+}
+])

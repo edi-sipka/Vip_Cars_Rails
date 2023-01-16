202b167cb7cd9b073680975048e009bbf4ba7c57
edi-sipka@gmail.com <edisipka@gmail.com>
2023-01-16 10:13:17 +0100

    Fix bugs with devise jwt config

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -11,6 +11,7 @@
 
     resource "*",
       headers: :any,
-      methods: [:get, :post, :put, :patch, :delete, :options, :head]
+      methods: [:get, :post, :put, :patch, :delete, :options, :head],
+      expose: ["Authorization"]
  end
 end

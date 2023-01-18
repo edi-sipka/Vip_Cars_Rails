a2e272d40c88b81954a03084441196d000f91242
Lena <summyalena@gmail.com>
2023-01-18 19:37:44 +0100

    make index reservations

--- app/controllers/api/v1/reservations_controller.rb
+++ app/controllers/api/v1/reservations_controller.rb
@@ -1,5 +1,6 @@
 class Api::V1::ReservationsController < ApplicationController
   before_action :authenticate_user!
-
-  def index; end
+  def index
+    render json: current_user.reservations.includes([:car]).order(id: :desc), status: :ok
+  end
 end

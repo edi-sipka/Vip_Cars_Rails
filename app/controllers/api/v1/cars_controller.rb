b5c189db300f4000b2f4ab0443a29c8452af0a27
Lena <summyalena@gmail.com>
2023-01-18 19:30:36 +0100

    create car controllers

--- app/controllers/api/v1/cars_controller.rb
+++ app/controllers/api/v1/cars_controller.rb
@@ -1,9 +1,19 @@
 class Api::V1::CarsController < ApplicationController
   before_action :set_car, only: [:show]
-
+  before_action :authenticate_user!
   def index
     @cars = Car.all
     render json: @cars, status: :ok
+  end
+  def create
+    return render json: { error: 'You are not allowed' }, status: :unauthorized unless @current_user.admin?
+
+    @car = Car.new(car_params)
+    if @car.save
+      render json: @car
+    else
+      render json: { error: 'Something went wrong' }, status: :bad_request
+    end
   end
 
   def show

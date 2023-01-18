4ba0a9f0989ebc29933885b36c88019c84801085
Lena <summyalena@gmail.com>
2023-01-18 19:45:28 +0100

    made updates to reservations

--- app/controllers/api/v1/reservations_controller.rb
+++ app/controllers/api/v1/reservations_controller.rb
@@ -12,6 +12,19 @@ class Api::V1::ReservationsController < ApplicationController
       render json: { error: 'Something went wrong' }, status: :bad_request
     end
   end
+  def destroy
+    reservation = Reservation.find(params[:id])
+
+    if reservation.destroy
+      render json: {
+        status: 200,
+        message: 'Reservation cancelled',
+        data: ReservationSerializer.new(reservation)
+      }, status: :ok
+    else
+      render json: { error: 'ERROR: Unable to cancel the reservation' }, status: :unprocessable_entity
+    end
+  end
   private
   def reservation_params
     params.require(:reservation).permit(:reservation_date, :returning_date, :car_id)

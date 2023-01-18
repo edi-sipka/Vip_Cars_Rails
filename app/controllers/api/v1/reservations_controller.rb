3f16861fbe33e7f537044a39663827832833f6ae
Lena <summyalena@gmail.com>
2023-01-18 19:39:47 +0100

    update reservation controllers

--- app/controllers/api/v1/reservations_controller.rb
+++ app/controllers/api/v1/reservations_controller.rb
@@ -3,4 +3,8 @@ class Api::V1::ReservationsController < ApplicationController
   def index
     render json: current_user.reservations.includes([:car]).order(id: :desc), status: :ok
   end
+  private
+  def reservation_params
+    params.require(:reservation).permit(:reservation_date, :returning_date, :car_id)
+  end
 end

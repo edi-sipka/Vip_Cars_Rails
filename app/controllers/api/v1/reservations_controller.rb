bf358946d8d2facd09b8e73b43739ca4847f5073
edi-sipka@gmail.com <edisipka@gmail.com>
2023-01-19 12:09:38 +0100

    Improve controllers cars

--- app/controllers/api/v1/reservations_controller.rb
+++ app/controllers/api/v1/reservations_controller.rb
@@ -1,12 +1,13 @@
 class Api::V1::ReservationsController < ApplicationController
   before_action :authenticate_user!
+ 
   def index
     render json: current_user.reservations.includes([:car]).order(id: :desc), status: :ok
   end
 
   def create
     @reservation = Reservation.new(reservation_params)
-    @reservation.user = @current_user
+    @reservation.user = current_user
     if @reservation.save
       render json: @reservation
     else
@@ -24,7 +25,7 @@ class Api::V1::ReservationsController < ApplicationController
         data: ReservationSerializer.new(reservation)
       }, status: :ok
     else
-      render json: { error: 'ERROR: Unable to cancel the reservation' }, status: :unprocessable_entity
+      render json: { error: 'ERROR: Unable to cancel the reservation'}, status: :unprocessable_entity
     end
   end
 
--- app/models/user.rb
+++ app/models/user.rb
@@ -11,4 +11,5 @@ class User < ApplicationRecord
   validates :name, presence: true
   validates :email, presence: true, uniqueness: true
   validates :password, presence: true, length: { in: 6..30 }
+  validates :role, presence: true
 end
